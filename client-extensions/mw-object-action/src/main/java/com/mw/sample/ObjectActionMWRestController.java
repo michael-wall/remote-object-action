45dca10bf083705b6e7b444efebe391b7a83413d
Michael Wall <Michael Wall@DESKTOP-FPKKMS7>
2025-07-03 08:56:05 +0100

    cleanup

--- client-extensions/mw-object-action/src/main/java/com/mw/sample/ObjectActionMWRestController.java
+++ client-extensions/mw-object-action/src/main/java/com/mw/sample/ObjectActionMWRestController.java
@@ -9,7 +9,6 @@ import com.liferay.client.extension.util.spring.boot3.BaseRestController;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
